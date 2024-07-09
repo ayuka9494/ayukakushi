3db09b0de0e395cb6f6c9dd035824e692a90f12a
ayuka9494 <167833160+ayuka9494@users.noreply.github.com>
2024-07-09 10:08:15 +0900

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -7,5 +7,5 @@ alert(Math.sprt(0/0));
 alert(Math.sprt(2+3));
 alert(Math.sprt('2'+'3'));
 alert(Math.sprt(Number('2')+Number('3')));
-alert(Math.sprt('2')+3);
+alert(Math.sprt('2'+3));
 alert(Math.sprt(Number('2')+3));
